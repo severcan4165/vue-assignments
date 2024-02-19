4adeef076bb379a0cf4f81dd837ea9163aafa301
salih <salihsevercann@gmail.com>
2024-02-19 22:08:20 +0300

    counter

---
+++ vuex-counter/src/components/Result.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p class="counter-container"> Sayaç : {{ counter }}</p>
+  </div>
+</template>
+<script>
+  export default {
+    
+    computed:{
+      counter(){
+        return this.$store.state.counter
+      }
+  
+    }
+  }
+
+</script>
+<style scoped>
+  .counter-container{
+    border: 1px solid #aa7f08;
+    padding: 10px 5px;
+    background-color: #fbbd08;
+    font-size: 20px;
+    color: #000;
+  }
+</style>
