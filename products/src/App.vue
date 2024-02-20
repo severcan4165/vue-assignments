5b283c260c2c7c29c20dc4f82a662ddd73782171
salih <salihsevercann@gmail.com>
2024-02-20 20:21:19 +0300

    product-initial-push

---
+++ products/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div >
+  <app-header></app-header>
+    <router-view></router-view>
+  <app-footer></app-footer>
+
+</div>
+
+</template>
+
+<script>
+import Header from "./components/shared/Header.vue"
+import Footer from "./components/shared/Footer.vue"
+import ProductPurchase from "./components/products/ProductPurchase.vue"
+import ProductSell from "./components/products/ProductSell.vue"
+import ProductList from "./components/products/ProductList.vue"
+
+export default {
+ components:{
+    appHeader:Header,
+    appFooter:Footer,
+    // appProductPurchase:ProductPurchase
+    appContainer:ProductList
+ },
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
